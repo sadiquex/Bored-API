c046f1d2f2394f0e4ca7f8cb98e8a4a8768eaeac
sadiquex <abubakasaddik1@gmail.com>
2022-07-20 23:04:58 +0000

    fetched data from api

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 // http://www.boredapi.com/api/activity/
 
+const storage = document.querySelector('.output')
+
 async function doSomething() {
     const response = await fetch('http://www.boredapi.com/api/activity/')
-      .then(response => response.json())
-      .then(data => console.log(data.activity));
+    // console.log(response.json())
 
+    // got json data here
+      .then(response => response.json())
+    //  manipulate json data for what we want
+      .then(data => {return data.activity});
+
+    // store the data into html
+    storage.textContent = response;
+      
 }
 
 doSomething()
