3687755b3dda4348ae29f24ef9fa3cc9fb81c130
sadiquex <abubakasaddik1@gmail.com>
2022-07-22 21:38:29 +0000

    added refresh button

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 // http://www.boredapi.com/api/activity/
+// https://jsonplaceholder.typicode.com/posts
 
 const storage = document.querySelector('.output')
+const category = document.querySelector(".category");
+const button = document.getElementById("btn");
+
+button.addEventListener('click', () => {
+  location.reload()
+})
 
 async function doSomething() {
-    const response = await fetch('https://www.boredapi.com/api/activity/', {mode: 'cors'})
-    
-    // console.log(response.json())
+    const response = await fetch("http://www.boredapi.com/api/activity/", {
+      mode: "cors",
+    })
+      // console.log(response.json())
 
-    // got json data here
-      .then(response => response.json())
-    //  manipulate json data for what we want
-      .then(data => {return data.activity});
+      // got json data here
+      .then((response) => response.json())
+      //  manipulate json data for what we want
+      .then((data) => {
+        activity = data.activity;
+        // category = data.type;
+        // console.log(activity);
+      });
 
 
     // store the data into html
-    storage.innerText = response;
+    storage.innerText = activity;
       
 }
 
 doSomething()
+
+
+// add a reload button
